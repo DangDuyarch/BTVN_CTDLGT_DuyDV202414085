6b6ce13cae667a6ffda4445535fc712052707a5e
ĐẶNG VĂN DUY <Duydang792006@gmail.com>
2026-05-29 17:11:49 +0700

    Them vai cmt, sua lai cach in ra cay nhi phan

--- BTVN_Tuan13_Caynhiphantimkiem.cpp
+++ BTVN_Tuan13_Caynhiphantimkiem.cpp
@@ -2,6 +2,8 @@
 
 // Cài đặt cây nhị phân bằng lưu trữ kế tiếp, các node rỗng được biểu diễn bởi gtri 0.
 
+// Quy tắc lập cây nhị phân hiện tại là: nếu nhỏ hơn hoặc bằng node hiện tại thì thêm vào con bên trái, nếu lớn hơn nút hiện tại thì bên phải
+
 #include <iostream>
 using namespace std;
 
@@ -10,26 +12,38 @@ void add_nodeBST(int tree[], int max, int a) {    // Hàm thêm node vào cây
     while (i < max) {              
         if (tree[i] == 0)  // Thêm giá trị vào node rỗng
         { tree[i] = a;
-            return;
-}
-        if (a < tree[i])  i = 2 * i + 1;        // Nếu a nhỏ hơn node hiện tại đan xét -> xét sang con bên trái
-        else if (a > tree[i]) i = 2 * i + 2;    // Nếu a lớn hơn thì xét sang phải
+            return;  }
+        if (a <= tree[i])  i = 2 * i + 1;        // Nếu a nhỏ hơn node hiện tại đan xét -> xét sang con bên trái
+        else if (a > tree[i]) i = 2 * i + 2;    // Nếu a lớn hơn thì xét sang phải
         else return;
     }
 }
+void search_BST(int tree[], int max, int find) {    // Hàm tìm kiếm 1 giá trị trong cây nhị phân 
+    int i = 0; 
+    while (i < max) {      // tạo vòng lặp từ đàu đến cuối cây 
 
-int search_BST(int tree[], int max, int find) {
-    int i = 0; // Luôn bắt đầu tìm kiếm từ nút gốc (index 0)
-    
-    while (i < max) {
-        if (tree[i] == 0)  return -1; 
-        if (tree[i] == find) return i; // Trả về vị trí  tìm thấy
-        if (find < tree[i]) {  // Nếu giá trị cần tìm lớn hơn nút hiện tại -> Rẽ sang phải
-            i = 2 * i + 1;  }
-        else { i = 2 * i + 2; }
-    }
-    return -1; // Trả về -1 nếu chạy hết mảng mà vẫn không thấy
+    if (tree[i] == 0) {  cout << "Khong tim thay gia tri " << find << endl;   // Kh thấy thì in ra thông báo tương tự
+            return; }
+    if (tree[i] == find) { cout << "Tim thay " << find << " tai vi tri: " << i << endl;  // Tìm thấy cũng in ra thông báo tương tự
+            return;   }
+
+
+    if (find <= tree[i]) {  // Nếu giá trị cần tìm lớn hơn nút hiện tại -> Rẽ sang phải
+        i = 2 * i + 1;  }
+    else { i = 2 * i + 2; }  // ngược lại thì sang trái
 }
+    cout << "Khong tim thay gia tri " << find << endl;   // Lặp hết mà kh thấy thì in ra kh thấy
+}
+
+void Print_BST(int tree[], int max) {
+    int p = -1;
+    for (int i = max - 1; i >= 0; i--) {     // Tìm ptu cuối cùng có nghĩa
+        if (tree[i] != 0) { p = i;   break;  }  } // Tìm phần tử khác rỗng cuối cùng và lưu vị trí
+
+    for (int i = 0; i <= p; i++) {   cout << tree[i] << " "; }       // In cây từ đầu đến vtri đó
+    cout << endl;
+}
+
 
 int main() {
     int tree[100] = {0};  // tạo mảng 100 ptu trong đó 0 đại diện cho node rỗng
@@ -38,29 +52,13 @@ int main() {
 
     for (int i = 0; i < n; i++) {
     add_nodeBST(tree, 100, arr[i]);   }    // Dùng hàm đã tạo để add từng node, tạo thành cây nhị phân tìm kiếm
-
-    cout << "Cay sau khi sap xep:\n";
-    for (int i = 0; i < 100; i++) {
-    cout << tree[i] << " ";
-    }
+    Print_BST(tree, 100);
 
     add_nodeBST(tree, 100, 1777);       // thêm ptu vào cây nhị phân tìm kiếm
-    cout << "Cay sau khi them ptu :\n";
-    for (int i = 0; i < 100; i++) { 
-    cout << tree[i] << " ";
-    }
-    cout << endl;
+    Print_BST(tree, 100);
 
-int can_tim = 2004;   // thu tìm kiếm 1 giá trị 
-    int vi_tri = search_BST(tree, 100, can_tim);
-    if (vi_tri != -1) { cout << "Tim thay so " << can_tim << " tai vi tri index: " << vi_tri << endl; } 
-    else {  cout << "Khong tim thay so " << can_tim << " trong cay!" << endl;    }
-
-    int can_tim2 = 1111;
-    if (search_BST(tree, 100, can_tim2) == -1) {
-        cout << "Chinh xac: So " << can_tim2 << " khong ton tai tren cay.\n";
-    }
-
+    search_BST(tree, 100, 2004); 
+    search_BST(tree, 100, 1111);
 
     return 0;
 }
