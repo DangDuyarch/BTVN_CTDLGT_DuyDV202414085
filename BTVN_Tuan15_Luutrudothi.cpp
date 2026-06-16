9616dde94cc52f2dbfa4ea2650e7fb2247b221d2
ĐẶNG VĂN DUY <Duydang792006@gmail.com>
2026-06-17 00:06:54 +0700

    Bo sung mtr, ds, them duong di cac tinh

--- BTVN_Tuan15_Luutrudothi.cpp
+++ BTVN_Tuan15_Luutrudothi.cpp
@@ -3,42 +3,65 @@
 #include <queue>
 using namespace std;
 
-const int Sotinh_max = 11;
-const string vertexNames[Sotinh_max] = {
+const int Sotinh_max = 11;    // khai bao 11 tinh theo de bai
+const string DSTinh[Sotinh_max] = {     // 11 tinh thoe bai
     "Ha Noi", "Thai Nguyen", "Bac Ninh", "Bac Giang",
     "Uong Bi", "Hai Phong", "Hai Duong", "Hung Yen", "Phu Ly", "Hoa Binh", "Son Tay"
 };
 
-struct Matran {
+struct Matran {   // khai báo
     int Sotinhxet;
     int matrix[Sotinh_max][Sotinh_max];
 };
 
-void Khoitaomatran(Matran &g, int n) {
+void Khoitaomatran(Matran &g, int n) { // tạo mtr trống (0)
     g.Sotinhxet = n;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            g.matrix[i][j] = 0;
-        }
-    }
+    for (int i = 0; i < n; i++) {     // mtr trống
+        for (int j = 0; j < n; j++)  g.matrix[i][j] = 0; }
 }
 
-struct Nut {
+void ThemDuongMaTran(Matran &g, int u, int v) {
+    g.matrix[u][v] = 1;
+    g.matrix[v][u] = 1; }
+
+
+
+
+struct Nut {       // Định nghĩa node
     int maTuyenTinh;    
-    Nut* tiepTheo;      
-};
+    Nut* tiepTheo;      };
 
-struct DanhSachDoThi {
+struct DanhSachDoThi {   // Định nghĩa ds lien ke
     int Sotinhxet;
-    Nut* dau[Sotinh_max]; 
-};
+    Nut* dau[Sotinh_max]; };
 
-void Khoitaods(DanhSachDoThi &dt, int n) {
+void Khoitaods(DanhSachDoThi &dt, int n) {  // DS trống (null)
     dt.Sotinhxet = n;
-    for (int i = 0; i < n; i++) {
-        dt.dau[i] = nullptr;
-    }
+    for (int i = 0; i < n; i++)  dt.dau[i] = nullptr;
+}
+
+void ThemDuongDanhSach(DanhSachDoThi &dt, int u, int v) {
+   // ham ket noi 2 node u voi v, tinh u co duogn den v và ngược lại
+    Nut* nutMoi1 = new Nut;     // Thêm v vào danh sách của u
+    nutMoi1->maTuyenTinh = v;
+    nutMoi1->tiepTheo = dt.dau[u];
+    dt.dau[u] = nutMoi1;
+
+    Nut* nutMoi2 = new Nut;   // Thêm u vào danh sách của v 
+    nutMoi2->maTuyenTinh = u;
+    nutMoi2->tiepTheo = dt.dau[v];
+    dt.dau[v] = nutMoi2;
 }
 
 int main() {
-    return 0;}
+    Matran g;
+    Khoitaomatran(g, 3);
+    cout << "Truoc khi them duong (HN-TN): " << g.matrix[0][1] << endl;
+    ThemDuongMaTran(g, 0, 1); 
+    
+    cout << "Sau khi them duong: " << endl;
+    cout << "Ha Noi -> Thai Nguyen: " << g.matrix[0][1] << endl;
+    cout << "Thai Nguyen -> Ha Noi: " << g.matrix[1][0] << endl;
+    
+    return 0;
+}
