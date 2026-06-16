a3a99cd3644fd22f8ef3c09d3127380260d69023
ĐẶNG VĂN DUY <Duydang792006@gmail.com>
2026-06-17 00:31:48 +0700

    Them ma duogn di, cap nhat dl, them ham in mtr

--- BTVN_Tuan15_Luutrudothi.cpp
+++ BTVN_Tuan15_Luutrudothi.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <string>
 #include <queue>
+#include <iomanip>
 using namespace std;
 
 const int Sotinh_max = 11;    // khai bao 11 tinh theo de bai
 const string DSTinh[Sotinh_max] = {     // 11 tinh thoe bai
-    "Ha Noi", "Thai Nguyen", "Bac Ninh", "Bac Giang",
-    "Uong Bi", "Hai Phong", "Hai Duong", "Hung Yen", "Phu Ly", "Hoa Binh", "Son Tay"
-};
+    "HN", "TN", "BN", "BG", "UB", "HP", "HD", "HY", "PL", "HB", "ST"};
+// Các ký hiệu tương đương : Hà Nội, Thái Nguyên, Bắc Ninh,... theo đề bài
 
-struct Matran {   // khai báo
+struct Matran {   // khai báo
     int Sotinhxet;
     int matrix[Sotinh_max][Sotinh_max];
 };
@@ -20,10 +20,9 @@ void Khoitaomatran(Matran &g, int n) { // tạo mtr trống (0)
         for (int j = 0; j < n; j++)  g.matrix[i][j] = 0; }
 }
 
-void ThemDuongMaTran(Matran &g, int u, int v) {
-    g.matrix[u][v] = 1;
-    g.matrix[v][u] = 1; }
-
+void ThemDuongMaTran(Matran &g, int u, int v, int matuyen) {
+    g.matrix[u][v] = matuyen;  // cap nhat lai, khph ton tai ma la tuyen duong
+    g.matrix[v][u] = matuyen; }
 
 
 
@@ -40,7 +39,7 @@ void Khoitaods(DanhSachDoThi &dt, int n) {  // DS trống (null)
     for (int i = 0; i < n; i++)  dt.dau[i] = nullptr;
 }
 
-void ThemDuongDanhSach(DanhSachDoThi &dt, int u, int v) {
+void ThemDuongDanhSach(DanhSachDoThi &dt, int u, int v, int maTuyen) { // bo sung
    // ham ket noi 2 node u voi v, tinh u co duogn den v và ngược lại
     Nut* nutMoi1 = new Nut;     // Thêm v vào danh sách của u
     nutMoi1->maTuyenTinh = v;
@@ -53,15 +52,38 @@ void ThemDuongDanhSach(DanhSachDoThi &dt, int u, int v) {
     dt.dau[v] = nutMoi2;
 }
 
+
+void InMaTran(Matran g) {
+    cout << setw(15) << " ";
+  for (int j = 0; j < g.Sotinhxet; j++) {   cout << setw(15) << DSTinh[j]; }  // in ten tỉnh theo hàng
+    cout << endl;
+
+    for (int i = 0; i < g.Sotinhxet; i++) {
+        cout << setw(15) << DSTinh[i];   // in ten tinh theo cột
+        for (int j = 0; j < g.Sotinhxet; j++) {
+            if (g.matrix[i][j] == 0)  cout << setw(15) << "0";   // kh co đường đi
+            else {
+                string tenDuong = "D" + to_string(g.matrix[i][j]);
+                cout << setw(15) << tenDuong; } }    // In ra mã đường
+        cout << endl;
+    }   }
+
 int main() {
-    Matran g;
-    Khoitaomatran(g, 3);
-    cout << "Truoc khi them duong (HN-TN): " << g.matrix[0][1] << endl;
-    ThemDuongMaTran(g, 0, 1); 
-    
-    cout << "Sau khi them duong: " << endl;
-    cout << "Ha Noi -> Thai Nguyen: " << g.matrix[0][1] << endl;
-    cout << "Thai Nguyen -> Ha Noi: " << g.matrix[1][0] << endl;
-    
+    int n = Sotinh_max;
+    Matran g; 
+    Khoitaomatran(g, n);
+    DanhSachDoThi dt; 
+    Khoitaods(dt, n);
+
+  
+    int CacTuyenDuong[][3] = { {0, 6, 1},  {6, 7, 2}, {7, 8, 3},  {0, 8, 4}, {0, 9, 5},   
+        {0, 10, 6}, {0, 1, 7},  {0, 2, 8},  {2, 3, 9}, {3, 4, 10},  {2, 4, 11},  {4, 5, 12},{6, 5, 13}  };
+// Nạp dữ liệu vào cấu trúc ma trận 
+
+    int soTuyen = sizeof(CacTuyenDuong) / sizeof(CacTuyenDuong[0]);
+    for(int i = 0; i < soTuyen; i++) { ThemDuongMaTran(g, CacTuyenDuong[i][0], CacTuyenDuong[i][1], CacTuyenDuong[i][2]);}
+
+
+    InMaTran(g);
     return 0;
 }
